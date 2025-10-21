94b955922165b94985be78d06a321632d97ce4ea
UncleMarvo <eddie_heff@yahoo.com>
2025-10-21 10:56:00 +0100

    Register OnboardingPage and PermissionGuidePage routes

--- mobile/Rascor.App/AppShell.xaml.cs
+++ mobile/Rascor.App/AppShell.xaml.cs
@@ -1,3 +1,5 @@
+using Rascor.App.Pages;
+
 namespace Rascor.App;
 
 public partial class AppShell : Shell
@@ -5,5 +7,9 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
+        
+        // Register routes for modal/push navigation
+        Routing.RegisterRoute(nameof(OnboardingPage), typeof(OnboardingPage));
+        Routing.RegisterRoute(nameof(PermissionGuidePage), typeof(PermissionGuidePage));
     }
 }
